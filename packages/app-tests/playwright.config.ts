fef68589c0b5ea67bb750805d5700049a0c2b974
serge <sergewilfried@outlook.fr>
2023-11-30 17:50:05 +0000

    chore(playwright.config.ts): increase timeout to 90 seconds for better test stability

--- packages/app-tests/playwright.config.ts
+++ packages/app-tests/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     video: 'retain-on-failure',
   },
-
+  timeout: 90000,
   /* Configure projects for major browsers */
   projects: [
     {
