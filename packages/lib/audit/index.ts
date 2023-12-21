fcb81a47775262ebe0f6dff686e94d81ec66c217
serge <sergewilfried@outlook.fr>
2023-12-21 20:02:43 +0000

    fix(audit/index.ts): fix linting issues and add missing type annotations for functions feat(audit/index.ts): add support for ENABLE_AUDIT_LOGS_REPORT environment variable to enable/disable audit logs reporting feat(audit/index.ts): add getViewerToken function to retrieve viewer token from Retraced

--- packages/lib/audit/index.ts
+++ packages/lib/audit/index.ts
@@ -4,6 +4,9 @@ import type { User } from 'next-auth';
 
 import type { AuditLogsEventType } from '../types/audit-logs';
 
+/**
+ * Represents a request object.
+ */
 type Request = {
   action: AuditLogsEventType;
   user: User;
@@ -13,11 +16,20 @@ type Request = {
 const RETRACED_API_KEY = process.env.RETRACED_API_KEY ?? '';
 const RETRACED_PROJECT_ID = process.env.RETRACED_PROJECT_ID ?? '';
 const RETRACED_URL = process.env.RETRACED_URL ?? '';
-
+const ENABLE_AUDIT_LOGS_REPORT = process.env.ENABLE_AUDIT_LOGS_REPORT ?? '';
 let retracedClient: Client;
 
+/**
+ * Retrieves the Retraced client.
+ * @returns The Retraced client if all required environment variables are present, otherwise returns undefined.
+ */
 const getRetracedClient = () => {
-  if (!RETRACED_API_KEY || !RETRACED_PROJECT_ID || !RETRACED_URL) {
+  if (
+    !RETRACED_API_KEY ||
+    !RETRACED_PROJECT_ID ||
+    !RETRACED_URL ||
+    ENABLE_AUDIT_LOGS_REPORT !== 'true'
+  ) {
     return;
   }
 
@@ -32,6 +44,11 @@ const getRetracedClient = () => {
   return retracedClient;
 };
 
+/**
+ * Sends an audit event to Retraced.
+ * @param request - The request object containing action, user, and crud information.
+ * @returns A promise that resolves with the reported event.
+ */
 export const sendAudit = async (request: Request) => {
   const retracedClient = getRetracedClient();
 
@@ -58,6 +75,12 @@ export const sendAudit = async (request: Request) => {
   return await retracedClient.reportEvent(event);
 };
 
+/**
+ * Retrieves a viewer token from Retraced.
+ * @param groupId - The ID of the group.
+ * @param actorId - The ID of the actor.
+ * @returns A promise that resolves with the viewer token.
+ */
 export const getViewerToken = async (groupId: string, actorId: string) => {
   const retracedClient = getRetracedClient();
 
