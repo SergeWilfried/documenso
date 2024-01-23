30fd758f19ad9e32987491dd64c724f4913f9039
serge <sergewilfried@outlook.fr>
2024-01-23 09:11:15 +0000

    feat(notifications): add Twilio constants for account token, account SID, and phone number chore(package.json): add Twilio package as a dependency

---
+++ packages/lib/constants/notifications.ts
@@ -0,0 +1,3 @@
+export const TWILLIO_ACCOUNT_TOKEN = process.env.TWILLIO_ACCOUNT_TOKEN || '';
+export const TWILLIO_ACCOUNT_SID = process.env.TWILLIO_ACCOUNT_SID || '';
+export const TWILLIO_PHONE_NUMBER = process.env.TWILLIO_PHONE_NUMBER || '';
