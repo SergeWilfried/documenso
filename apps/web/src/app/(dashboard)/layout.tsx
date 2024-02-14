3cbe2bcffc0baa500f89768bf41f7c6c6093fa02
serge <sergewilfried@outlook.fr>
2024-02-14 21:01:12 +0000

    fix: update platform name

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ import { NextAuthProvider } from '~/providers/next-auth';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Documenso',
+    template: '%s | MonTampon',
     default: 'Dashboard',
   },
 };
